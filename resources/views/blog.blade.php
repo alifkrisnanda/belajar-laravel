7810ebaf71d07b8f5774435880b43294b5e07a03
Alif Krisnanda <alifkrisnanda023@gmail.com>
2019-11-30 21:08:28 +0700

    commit pertama laravel

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar Laravel - Alif Krisnanda</title>
+</head>
+<body>
+ 
+	<h3>Alifkrisnanda</h3>
+	<p>Seri Belajar Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+ 
+</body>
+</html>
